babfab05cdc521cdd45dc29903f2fcb0748cc22a
Mark Smukler <smukler.mark@gmail.com>
2015-10-24 18:47:24 -0700

    small change to confirmation page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+members = Member.create([
+	{
+		first_name: "Mark",
+		last_name: "Smukler",
+		zipcode: "07042",
+		gender: "MALE",
+		email: "smukler.mark@gmail.com",
+		password: "dipmark"
+	},
+	{
+		first_name: "Dylan",
+		last_name: "Bushnell",
+		zipcode: "90210",
+		gender: "MALE",
+		email: "dylan@thedipapp.com",
+		password: "dipdylan"
+	},
+	{
+		first_name: "Curt",
+		last_name: "Selak",
+		zipcode: "10010",
+		gender: "MALE",
+		email: "curt@thedipapp.com",
+		password: "dipcurt"
+	}
+])
