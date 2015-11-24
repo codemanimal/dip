9d80fc8a8b69a85f759b901e8c907a1299baae3c
Mark Smukler <smukler.mark@gmail.com>
2015-11-24 17:14:10 -0500

    fixed submit action and designed confirmation page

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,12 +1,14 @@
 class Member < ActiveRecord::Base
 
-	has_secure_password
+	# has_secure_password
 
-	validates :email, :password_digest,
-						presence: true,
-						uniqueness: { case_sensitive: false }
+	
 
-	validates :first_name, :last_name, :zipcode, :gender,
-						presence: true
+	# validates :email, :password_digest,
+	# 					presence: true,
+	# 					uniqueness: { case_sensitive: false }
+
+	# validates :first_name, :last_name, :zipcode, :gender,
+	# 					presence: true
 
 end
